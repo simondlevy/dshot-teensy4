adb9d6625c3def532ac2c05cd025d871a7a8450a
Simon D. Levy <simon.d.levy@gmail.com>
2026-01-31 17:35:24 -0500

    Cleanup

--- examples/Test/Test.ino
+++ examples/Test/Test.ino
@@ -21,18 +21,6 @@
 
 static DshotTeensy4 _motors = DshotTeensy4({6, 5, 4, 3});
 
-static uint32_t current_time, prev_time;
-
-static void loopRate(int freq) {
-
-    float invFreq = 1.0/freq*1000000.0;
-    unsigned long checker = micros();
-
-    while (invFreq > (checker - current_time)) {
-        checker = micros();
-    }
-}
-
 static float MAX = 0.5;
 static float INC = 1e-5;
 
@@ -84,32 +72,25 @@ static float inputGet()
     return val;
 }
 
-static void inputInit(void)
-{
-    dir = +1;
-}
 void setup()
 {
     Serial.begin(500000); 
 
     delay(500);
 
-    inputInit();
+    dir = +1;
 
     _motors.arm(); 
 }
 
 void loop()
 {
-    prev_time = current_time;      
-    current_time = micros();      
-
     const float inp = inputGet();
 
     float motorvals[4] = {inp, inp, inp, inp};
 
     _motors.run(inp > 0, motorvals);
 
-    loopRate(2000); 
+    delay(1);
 }
 
