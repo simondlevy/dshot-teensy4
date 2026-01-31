ed811e2b7e56a0e2270d8615f85d2f6d1075c10b
Simon D. Levy <simon.d.levy@gmail.com>
2026-01-31 17:32:38 -0500

    Test.ino working

--- examples/Test/Test.ino
+++ examples/Test/Test.ino
@@ -1,3 +1,22 @@
+/*
+   Test ESCs.  MAKE SURE PROPELLERS ARE REMOVED!!!
+
+   This file is part of DSHOT-Teensy4.
+
+   DSHOT-Teensy4 is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the Free
+   Software Foundation, either version 3 of the License, or (at your option)
+   any later version.
+
+   DSHOT-Teensy4 is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+   more details.
+
+   You should have received a copy of the GNU General Public License along with
+   DSHOT-Teensy4. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <dshot-teensy4.hpp>  
 
 static DshotTeensy4 _motors = DshotTeensy4({6, 5, 4, 3});
@@ -14,12 +33,68 @@ static void loopRate(int freq) {
     }
 }
 
+static float MAX = 0.5;
+static float INC = 1e-5;
 
+static bool running;
+
+static float val;
+static float dir;
+
+void serialEvent()
+{
+  while (Serial.available()) {
+    Serial.read();
+  }
+
+  running = !running;
+}
+
+static void prompt()
+{
+    static uint32_t tprev;
+    const uint32_t tcurr = millis();
+    if (tcurr - tprev > 1000) {
+      tprev = tcurr;
+      Serial.print("Hit Enter to ");
+      Serial.println(running ? "stop" : "start");
+    }
+}
+
+static float inputGet()
+{
+    prompt();
+
+    if (running) {
+
+        val += INC * dir;
+
+        if (val >= MAX) {
+            dir = -1;
+        }
+
+        if (val <= 0) {
+            dir = +1;
+        }
+    }
+    else {
+        val = 0;
+    }
+
+    return val;
+}
+
+static void inputInit(void)
+{
+    dir = +1;
+}
 void setup()
 {
     Serial.begin(500000); 
 
     delay(500);
+
+    inputInit();
 
     _motors.arm(); 
 }
@@ -29,9 +104,11 @@ void loop()
     prev_time = current_time;      
     current_time = micros();      
 
-    float motorvals[4] = {};
+    const float inp = inputGet();
 
-    _motors.run(false, motorvals);
+    float motorvals[4] = {inp, inp, inp, inp};
+
+    _motors.run(inp > 0, motorvals);
 
     loopRate(2000); 
 }
